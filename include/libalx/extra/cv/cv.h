f3de7e44f0f1f5cc1a9a91b65e214bbb212cc470
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-03-25 16:43:21 +0100

    extra/cv:  provide header wrapper <libalx/extra/cv/cv.h[pp]>

---
+++ include/libalx/extra/cv/cv.h
@@ -0,0 +1,62 @@
+/******************************************************************************
+ *	Copyright (C) 2020	Alejandro Colomar Andrés		      *
+ *	SPDX-License-Identifier:	LGPL-2.0-only			      *
+ ******************************************************************************/
+
+
+/******************************************************************************
+ ******* include guard ********************************************************
+ ******************************************************************************/
+#pragma once	/* libalx/extra/cv/cv.h */
+
+
+/******************************************************************************
+ ******* headers **************************************************************
+ ******************************************************************************/
+#include <libalx/extra/cv/alx/compare.h>
+#include <libalx/extra/cv/alx/gray.h>
+#include <libalx/extra/cv/alx/lines.h>
+#include <libalx/extra/cv/alx/max.h>
+#include <libalx/extra/cv/alx/mean.h>
+#include <libalx/extra/cv/alx/median.h>
+#include <libalx/extra/cv/alx/skeleton.h>
+#include <libalx/extra/cv/core/array/bitwise.h>
+#include <libalx/extra/cv/core/array/component.h>
+#include <libalx/extra/cv/core/array/minmax.h>
+#include <libalx/extra/cv/core/array/normalize.h>
+#include <libalx/extra/cv/core/contours/extract.h>
+#include <libalx/extra/cv/core/contours/init.h>
+#include <libalx/extra/cv/core/img/img.h>
+#include <libalx/extra/cv/core/pixel/pixel.h>
+#include <libalx/extra/cv/core/rect/rect.h>
+#include <libalx/extra/cv/core/roi/roi.h>
+#include <libalx/extra/cv/features2d/orb.h>
+#include <libalx/extra/cv/highgui/file.h>
+#include <libalx/extra/cv/highgui/window.h>
+#include <libalx/extra/cv/imgproc/features/edges.h>
+#include <libalx/extra/cv/imgproc/features/features.h>
+#include <libalx/extra/cv/imgproc/filter/border.h>
+#include <libalx/extra/cv/imgproc/filter/dilate_erode.h>
+#include <libalx/extra/cv/imgproc/filter/edges.h>
+#include <libalx/extra/cv/imgproc/filter/smooth.h>
+#include <libalx/extra/cv/imgproc/geometric/mirror.h>
+#include <libalx/extra/cv/imgproc/geometric/resize.h>
+#include <libalx/extra/cv/imgproc/geometric/rotate.h>
+#include <libalx/extra/cv/imgproc/histogram/hist.h>
+#include <libalx/extra/cv/imgproc/miscellaneous/color.h>
+#include <libalx/extra/cv/imgproc/miscellaneous/distance.h>
+#include <libalx/extra/cv/imgproc/miscellaneous/fill.h>
+#include <libalx/extra/cv/imgproc/miscellaneous/resolution.h>
+#include <libalx/extra/cv/imgproc/miscellaneous/threshold.h>
+#include <libalx/extra/cv/imgproc/shape/contour/contours.h>
+#include <libalx/extra/cv/imgproc/shape/contour/draw.h>
+#include <libalx/extra/cv/imgproc/shape/contour/sort.h>
+#include <libalx/extra/cv/imgproc/shape/rect.h>
+#include <libalx/extra/cv/types.h>
+#include <libalx/extra/cv/videoio/cam.h>
+#include <libalx/extra/cv/ximgproc/thinning.h>
+
+
+/******************************************************************************
+ ******* end of file **********************************************************
+ ******************************************************************************/
---
+++ include/libalx/extra/cv/cv.hpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+ *	Copyright (C) 2020	Alejandro Colomar Andrés		      *
+ *	SPDX-License-Identifier:	LGPL-2.0-only			      *
+ ******************************************************************************/
+
+
+/******************************************************************************
+ ******* include guard ********************************************************
+ ******************************************************************************/
+#pragma once	/* libalx/extra/cv/cv.hpp */
+
+
+/******************************************************************************
+ ******* headers **************************************************************
+ ******************************************************************************/
+#include <libalx/extra/cv/alx/compare.hpp>
+#include <libalx/extra/cv/alx/gray.hpp>
+#include <libalx/extra/cv/alx/lines.hpp>
+#include <libalx/extra/cv/alx/max.hpp>
+#include <libalx/extra/cv/alx/mean.hpp>
+#include <libalx/extra/cv/alx/median.hpp>
+#include <libalx/extra/cv/alx/skeleton.hpp>
+#include <libalx/extra/cv/core/array/bitwise.hpp>
+#include <libalx/extra/cv/core/array/component.hpp>
+#include <libalx/extra/cv/core/array/minmax.hpp>
+#include <libalx/extra/cv/core/array/normalize.hpp>
+#include <libalx/extra/cv/core/contours/extract.hpp>
+#include <libalx/extra/cv/core/contours/init.hpp>
+#include <libalx/extra/cv/core/img/img.hpp>
+#include <libalx/extra/cv/core/pixel/pixel.hpp>
+#include <libalx/extra/cv/core/rect/rect.hpp>
+#include <libalx/extra/cv/core/roi/roi.hpp>
+#include <libalx/extra/cv/features2d/orb.hpp>
+#include <libalx/extra/cv/highgui/file.hpp>
+#include <libalx/extra/cv/highgui/window.hpp>
+#include <libalx/extra/cv/imgproc/features/edges.hpp>
+#include <libalx/extra/cv/imgproc/features/features.hpp>
+#include <libalx/extra/cv/imgproc/filter/border.hpp>
+#include <libalx/extra/cv/imgproc/filter/dilate_erode.hpp>
+#include <libalx/extra/cv/imgproc/filter/edges.hpp>
+#include <libalx/extra/cv/imgproc/filter/smooth.hpp>
+#include <libalx/extra/cv/imgproc/geometric/mirror.hpp>
+#include <libalx/extra/cv/imgproc/geometric/resize.hpp>
+#include <libalx/extra/cv/imgproc/geometric/rotate.hpp>
+#include <libalx/extra/cv/imgproc/histogram/hist.hpp>
+#include <libalx/extra/cv/imgproc/miscellaneous/color.hpp>
+#include <libalx/extra/cv/imgproc/miscellaneous/distance.hpp>
+#include <libalx/extra/cv/imgproc/miscellaneous/fill.hpp>
+#include <libalx/extra/cv/imgproc/miscellaneous/resolution.hpp>
+#include <libalx/extra/cv/imgproc/miscellaneous/threshold.hpp>
+#include <libalx/extra/cv/imgproc/shape/contour/contours.hpp>
+#include <libalx/extra/cv/imgproc/shape/contour/draw.hpp>
+#include <libalx/extra/cv/imgproc/shape/contour/sort.hpp>
+#include <libalx/extra/cv/imgproc/shape/rect.hpp>
+#include <libalx/extra/cv/videoio/cam.hpp>
+#include <libalx/extra/cv/ximgproc/thinning.hpp>
+
+
+/******************************************************************************
+ ******* end of file **********************************************************
+ ******************************************************************************/
