8b7f3eaf54464d73841588c073f4fb0b97c4209f
Alejandro Colomar <colomar.6.4.3@gmail.com>
2020-04-28 01:13:23 +0200

    Create subdirs for cblas levels

--- src/extra/zbar/zbar.c
+++ src/extra/zbar/zbar.c
@@ -56,7 +56,10 @@ int	alx_zbar_read	(ptrdiff_t bufsiz,
 
 	/* wrap image data */
 	image_zb = zbar_image_create();
+#pragma GCC diagnostic push	/* XXX: Why is this function so weird? */
+#pragma GCC diagnostic ignored	"-Wcast-align"
 	zbar_image_set_format(image_zb, *(int *)"GREY");
+#pragma GCC diagnostic pop
 	zbar_image_set_size(image_zb, cols, rows);
 	zbar_image_set_data(image_zb, imgdata, cols * rows, NULL);
 
